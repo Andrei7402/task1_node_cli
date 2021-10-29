69a5c04d8577f0d4618e0fdcb2a6aec62301edfb
Андрей <andrei7402@gmail.com>
2021-10-29 16:57:36 +0300

    feat: actionHandler implementation

--- index.js
+++ index.js
@@ -5,13 +5,33 @@ const chalk = require("chalk");
 const program = require("commander");
 const pckg = require("./package.json");
 
+const TaskTransform = require("./helpers/TaskTransform.class");
+
 const pipeline = util.promisify(stream.pipeline);
+const errorMessage = chalk.red.bold("✘ Erorr");
+const successMessage = chalk.green.bold("✔ Successful");
+const codeMessage = chalk.yellow.bold("Code");
 
+const actionHandler = async () => {
+    let { input, output, action } = program.opts();
 
-const actionHandler = async () => {}
+    try {
+        await pipeline(
+          input ? fs.createReadStream(input) : process.stdin,
+          new TaskTransform(action),
+          output ? fs.createWriteStream(output, { flags: "a" })
+            : process.stdout
+        );
+    
+        console.log('Task completed!');
+      } catch (e) {
+        process.stderr.write(`${errorMessage} ${e.message}\n`);
+        process.exit(1);
+      }
+}
 
 process.stdin.setEncoding("utf8");
-process.on("exit", (code) => log(`${codeMessage} ${code}`));
+process.on("exit", (code) => console.log(`${codeMessage} ${code}`));
 process.on("SIGINT", () => {
   process.exit(0);
 });
